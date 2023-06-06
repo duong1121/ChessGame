51efe66f717551ad7cc8c0f608317ba3a650955c
duong1121 <75771867+duong1121@users.noreply.github.com>
2023-06-06 20:48:54 +0700

    Delete Vua.cpp

--- source/Vua.cpp
+++
@@ -1,8 +0,0 @@
-#include"Vua.h"
-
-bool King::CheckNewPos(int DesCol, int DesRow, Pieces* Board[8][8]) {
-	if (DesCol - Pos.Col >= -1 && DesCol - Pos.Col <= 1 && DesRow - Pos.Row >= -1 && DesRow - Pos.Row <= 1)
-		return true;
-	return false;
-}
-
